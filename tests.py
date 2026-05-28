9c509357e010add18b744140341986e4b71b3461
cmatthey73 <chrismatthey1@gmail.com>
2026-05-28 07:51:56 +0000

    v2

--- tests.py
+++ tests.py
@@ -16,4 +16,40 @@ print("nb nace :", df["code"].n_unique())
 
 # %% prepare data
 
+from sklearn.model_selection import train_test_split
 
+train_df, tmp_df = train_test_split(df,  test_size=0.30, random_state=42)
+val_df, test_df  = train_test_split(tmp_df, test_size=0.50, random_state=42)
+
+X_train, y_train = train_df["label"].to_numpy(), train_df["code"].to_numpy()
+X_val, y_val = val_df["label"].to_numpy(), val_df["code"].to_numpy()
+X_test, y_test = test_df["label"].to_numpy(), test_df["code"].to_numpy()
+
+print(f"Train: {len(train_df)} | Val: {len(val_df)} | Test: {len(test_df)}")
+print(X_train[0])
+print(y_train[0])
+
+from sklearn.preprocessing import LabelEncoder
+
+encoder = LabelEncoder()
+encoder.fit(train_df['code'].to_numpy())
+all_codes  = set(df['code'])
+train_codes = set(train_df['code'])
+missing = all_codes - train_codes
+
+if missing:
+    print(f"WARNING: {len(missing)} code(s) missing from training set: {missing}")
+else:
+    print(f"OK — all {len(all_codes)} codes appear in the training set.")
+
+from torchTextClassifiers.value_encoder import ValueEncoder
+
+value_encoder = ValueEncoder(label_encoder=encoder)
+
+# %% tokenisation
+
+
+
+
+
+# %%
